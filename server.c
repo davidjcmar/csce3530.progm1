16f6ede1bd3f5d364c5273c206248b0d3654fb4c
davidjcmar <davidjcmar@gmail.com>
2015-09-23 22:26:35 -0500

    "add host/url parsing function""

--- server.c
+++ server.c
@@ -5,14 +5,29 @@
 #include <netinet/in.h>
 #define MESLEN 2048
 #define PORTNUM 56565
+/* parse url from client for GET request */
+void parse_client (char* message, char* url, char* host)
+{
+	int i,j;
+	memset(host,'\0',256);
+	memset(url,'\0',MESLEN-256);
+	for (i=0;message[i]!='\n';i++)
+	{
+		host[i]=message[i];
+	}
+	for(j=0;message[i]!='\0';i++,j++)
+	{
+		url[j]=message[i];
+	}
 
+}
 int main (void)
 {
 	int sock_descript, sock_cli_ser, size;
-	struct sockaddr_in server, client;
-	char message[MESLEN];
+	struct sockaddr_in server, client, proxy;
+	char message[MESLEN], url[MESLEN-256], host[256];
 
-	/* create socket */
+	/* create socket to client */
 	sock_descript=socket(AF_INET,SOCK_STREAM,0);
 	if (sock_descript==-1)
 	{
@@ -45,15 +60,19 @@ int main (void)
 		printf ("Connection not accepted.\n");
 		return 1;
 	}
-		printf ("Connection accepted.\n");
+	printf ("Connection accepted.\n");
 
-		/* hello client */
-		memset(message,'\0',MESLEN);
-		strcpy (message,"Enter a URL for which you want a HTTP request: ");
-		write (sock_cli_ser, message, strlen(message));
-		memset(message,'\0',MESLEN);
-		read (sock_cli_ser, message, MESLEN);
-		printf ("%s\n", message);
+	/* communicate with client */
+	memset(message,'\0',MESLEN);
+	strcpy (message,"Enter a URL for which you want a HTTP request: ");
+	write (sock_cli_ser, message, strlen(message));
+	memset(message,'\0',MESLEN);
+	read (sock_cli_ser, message, MESLEN);
+//	printf ("%s\n", message); // testing
+	parse_client (message, url, host);
+	printf ("url: %s\thost: %s\n",url,host);
+	/* create socket to inet */
+
 
 	return 0;
 }
